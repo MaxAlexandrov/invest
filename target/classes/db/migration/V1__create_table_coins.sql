954a5e344f03d1ce9db108b5337056f236409866
Maksim Aleksandrov <maximalexandrov87@gmail.com>
2023-10-02 20:31:31 +0600

    remove target from git; remove pgdata from git

--- target/classes/db/migration/V1__create_table_coins.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS coins
-(
-    date  timestamp,
-    name  varchar,
-    price float4,
-    PRIMARY KEY (date, name)
-);
--- target/classes/db/migration/V2__create_table_file_metadata.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS file_metadata
-(
-    hash                varchar PRIMARY KEY,
-    file_name           varchar,
-    processed_timestamp timestamp,
-    creation_date       timestamp,
-    last_modified       timestamp,
-    size                bigint
-);
